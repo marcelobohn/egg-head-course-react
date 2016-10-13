75bc9e0e9441b71cb584aa828dde7b14a9335991
Marcelo <marcelo.bohn@consyste.com.br>
2016-10-13 20:08:30 -0300

    insert component Profile in main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,15 +2,13 @@ require('normalize.css/normalize.css');
 require('styles/App.css');
 
 import React from 'react';
-
-let yeomanImage = require('../images/yeoman.png');
+import Profile from './Profile';
 
 class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+        <Profile name="Marcelo"/>
       </div>
     );
   }
